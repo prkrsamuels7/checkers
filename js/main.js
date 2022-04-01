bbbf65293bf21b07213853bf35a6a0f0ac083970
prkrsamuels7 <prkrsamuels@protonmail.com>
2022-04-01 16:50:26 -0700

    New Board and pieceEls

--- js/main.js
+++ js/main.js
@@ -1,104 +1,60 @@
-// document.querySelector('.play-again-btn').addEventListener('click', handleClick);
 
-// function handleClick(event) {
-//     console.log(event);
-// }
 
-class Piece {
-  constructor(color, position) {
-    this.color = color;
-    this.position = position;
-  }
-  checkMoves() {
 
-  }
-}
-
-let boardOccupied = {
-    '1': '.red',
-    '-1': '.black',
-    'null': ''
-}
-
-let board = [
-  [0, 1, 0, 1, 0, 1, 0 ,1],
-  [1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0 ,1],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0]
-  [-1, 0, -1, 0, -1, 0, -1, 0],
-  [0, -1, 0, -1, 0, -1, 0, -1],
-  [-1, 0, -1, 0, -1, 0, -1, 0]
+/*------------constants------------*/
+const NEW_BOARD = [
+	[0,  1,  0,  1,  0,  1,  0,  1],
+	[1,  0,  1,  0,  1,  0,  1,  0],
+	[0,  1,  0,  1,  0,  1,  0,  1],
+	[0,  0,  0,  0,  0,  0,  0,  0],
+	[0,  0,  0,  0,  0,  0,  0,  0],
+	[-1, 0, -1,  0, -1,  0, -1,  0],
+	[0, -1,  0, -1,  0, -1,  0, -1],
+	[-1, 0, -1,  0, -1,  0, -1,  0]
 ];
 
-var pieces = []
-// var board = [];
 
-// formula to get distance
-var distance = function(x1, x2, y1, y2) {
-  return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
-}
-
-class Pieces = {
-  constructor
-}
-
-
-let pieces = document.querySelectorAll('.piece');
-let currentPiece;
+/*------------state variables------------*/
+let board;
 let playerTurn;
+let redPieces = [];
+let blackPieces = [];
 
-document.querySelector('#board').addEventListener('click', handleClick)
 
+
+/*------------DOM elements------------*/
+const pieceEls = {
+  red: document.getElementsByClassName('red'),
+  black: document.getElementsByClassName('black')
+}
+
+/*------------event listeners------------*/
+document.querySelector('table').addEventListener('click', handleClick);
+
+/*------------functions------------*/
 init();
 
 function init() {
-  resetBoard();
-  checkBoardState();
+  board = NEW_BOARD;
+  playerTurn = -1;
+  getPlayerPieces();
 }
 
-function handleClick(event) {
-  let isPiece = event.target.classList[1];
-  if(!isPiece) return;
-  currentPiece = event.target;
-  console.log(currentPiece);
-  checkLegalMoves();
-}
+function getPlayerPieces() {
+  board.forEach((element) => {
+    element.forEach((square) => {
+      if(square === 1) blackPieces.push(square);
+      if(square === -1) redPieces.push(square);
+    });
+  })
+};
 
-function resetBoard() {
-  for(let i = 1; i < 9; i++) {
-    board.push(Array.from(document.querySelectorAll(`div div:nth-child(${i})`)));
-  };
-}
-
-function checkBoardState() {
-  board.forEach(function(element) {
-    element.forEach(function(square) {
-      if(square.classList.contains('.black-occupied')) {
-        square = '-1';
-      }
-    })
+function renderPieces() {
+  board.forEach((element) => {
   })
 }
 
-function checkLegalMoves() {}
-
-function listenForTurn() {
+function handleClick(evt) {
+  if(!evt.target.classList.contains('piece')) return;
+  console.log('clicked ME!')
 }
-
-// const inventors = [
-//   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-//   { first: 'Isaac', last: 'Newton', year: 1643, passed: 17-17 },
-//   { first: 'Galileo', last: 'Galilei', year: 1564, passed: 164-1 },
-//   { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-//   { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-//   { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-//   { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-//   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-//   { first: 'Ada', last: 'Lovelace', year: 1815, passed: 185-1 },
-//   { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-//   { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-//   { first: 'Hanna', last: 'Hammarström', year: 18-19, passed: 1909 }
-// ];
-
-// const inventorNames = inventors.map(({ first, last }) => ({ first, last }));
---
+++ js/piece.js
@@ -0,0 +1,12 @@
+// export default class Piece {
+//   constructor(pieceEl, position) {
+//     this.allowedToMove = true; // Every piece can move when game starts
+//     this.pieceEl = pieceEl; // Represents the linked DOM element
+//     this.position = position; // Position on board [row][col]
+//     this.player = ''; // Which player piece belongs to
+//     this.pieceEl.attr('id') >= 12 ? this.player = 1 : -1; // Map piece by id
+//     this.king = false; // King status
+//     this.move = function(tile) {}
+//   }
+// }
+
