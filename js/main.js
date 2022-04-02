422e5320c901f82aa022c8b5db8274e8df57aeae
Parker <prkrsamuels@protonmail.com>
2022-04-02 11:58:49 -0400

    render newBoard

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@
 
 /*------------constants------------*/
 const NEW_BOARD = [
-	[0,  1,  0,  1,  0,  1,  0,  1],
-	[1,  0,  1,  0,  1,  0,  1,  0],
-	[0,  1,  0,  1,  0,  1,  0,  1],
-	[0,  0,  0,  0,  0,  0,  0,  0],
-	[0,  0,  0,  0,  0,  0,  0,  0],
-	[-1, 0, -1,  0, -1,  0, -1,  0],
-	[0, -1,  0, -1,  0, -1,  0, -1],
-	[-1, 0, -1,  0, -1,  0, -1,  0]
+	[null,  1,     null,  1,     null,  1,     null,  1],
+	[1,     null,  1,     null,  1,     null,  1,     null],
+	[null,  1,     null,  1,     null,  1,     null,  1],
+	[0,     null,  0,     null,  0,     null,  0,     null],
+	[null,  0,     null,  0,     null,  0,     null,  0],
+	[-1,    null, -1,     null, -1,     null, -1,      null],
+	[null, -1,     null, -1,     null, -1,     null, -1],
+	[-1,    null, -1,     null, -1,     null, -1,     null]
 ];
-
 
 /*------------state variables------------*/
 let board;
@@ -20,9 +19,11 @@ let playerTurn;
 let redPieces = [];
 let blackPieces = [];
 let selectedPiece;
-const player = {
+const squareContains = {
   '-1': 'red',
-  '1': 'black'
+  '1': 'black',
+  '0': 'ds',
+  'null': 'ls',
 }
 
 
@@ -33,6 +34,8 @@ const pieceEls = {
   black: document.getElementsByClassName('black')
 }
 
+
+
 /*------------event listeners------------*/
 document.querySelector('table').addEventListener('click', handleClick);
 
@@ -41,8 +44,10 @@ init();
 
 function init() {
   board = NEW_BOARD;
+  createNewBoard();
   playerTurn = -1;
   getPlayerPieces();
+  render();
 }
 
 function getPlayerPieces() {
@@ -54,21 +59,65 @@ function getPlayerPieces() {
   })
 };
 
-function renderPieces() {
-  board.forEach((element) => {
+
+function render() {
+}
+
+
+/* Return if item clicked is not a piece.
+ Also return if it is not the turn of the player that clicked. Will allow player to switch
+ selected piece if it is their turn*/
+function handleClick(evt) {
+  if(!evt.target.classList.contains('piece')) return;
+  if(!evt.target.classList.contains(`${squareContains[playerTurn]}`)) return;
+  selectedPiece = evt.target;
+  console.log(selectedPiece);
+
+
+  // playerTurn *= -1;
+}
+
+/*---------------functions for creating the original board---------------- */
+function createLightSquare() {
+  let ls = document.createElement('td');
+  ls.classList.add('ls')
+  return ls;
+}
+
+function createDarkSquare() {
+  let ds = document.createElement('td');
+  ds.classList.add('ds');
+  return ds;
+}
+
+function createBlackPiece() {
+  let bp = document.createElement('td');
+  bp.classList.add('ds');
+  bp.innerHTML = '<p class="black piece"></p>';
+  return bp;
+}
+
+function createWhitePiece() {
+  let wp = document.createElement('td');
+  wp.classList.add('ds');
+  wp.innerHTML = '<p class="red piece"></p>';
+  return wp;
+}
+
+function createNewBoard() {
+  board.forEach(function(element, index) {
+    element.forEach(function(square) {
+      if(square === null) {
+        document.getElementById(`row${index}`).appendChild(createLightSquare());
+      } else if(square === 0) {
+         document.getElementById(`row${index}`).appendChild(createDarkSquare());
+      } else if (square === 1) {
+         document.getElementById(`row${index}`).appendChild(createBlackPiece());
+      } else if (square === -1) {
+        document.getElementById(`row${index}`).appendChild(createWhitePiece());
+      }
+    })
   })
 }
 
-
-// Return if item clicked is not a piece.
-//  Also return if it is not the turn of the player that clicked
-function handleClick(evt) {
-  if(!evt.target.classList.contains('piece')) return;
-  let targetID = evt.target.id.replace('p', '');
-  targetID <= 12 ? targetID = 1 : targetID = -1
-  if(targetID !== playerTurn) return;
-  console.log(targetID);
-
-
-  playerTurn *= -1;
-}
+/*------------------piece movement---------------*/
--- js/piece.js
+++ js/piece.js
@@ -10,3 +10,83 @@
 //   }
 // }
 
+{/* <tr id="row0">
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p0"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p1"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p2"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p3"></p></td>
+      </tr>
+      <tr id="row1">
+          <td class="ds"><p class="black piece" id="p4"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p5"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p6"></p></td>
+          <td class="ls"></td>
+          <td class="ds"><p class="black piece" id="p7"></p></td>
+          <td class="ls"></td>
+      </tr>
+      <tr id="row2">
+        <td class="ls"></td>
+        <td class="ds"><p class="black piece" id="p8"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="black piece" id="p9"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="black piece" id="p10"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="black piece" id="p11"></p></td>
+      </tr>
+      <tr id="row3">
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+      </tr>
+      <tr id="row4">
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+        <td class="ls"></td>
+        <td class="ds"></td>
+      </tr>
+      <tr id="row5">
+        <td class="ds"><p class="red piece" id="p12"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p13"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p14"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p15"></p></td>
+        <td class="ls"></td>
+      </tr>
+      <tr id="row6">
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p16"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p17"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p18"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p19"></p></td>
+      </tr>
+      <tr id="row7">
+        <td class="ds"><p class="red piece" id="p20"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p21"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p22"></p></td>
+        <td class="ls"></td>
+        <td class="ds"><p class="red piece" id="p23"></p></td>
+        <td class="ls"></td>
+      </tr> */}
